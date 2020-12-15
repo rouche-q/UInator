52b5eae33cb6cb5028b5f5a6a8ee4837eed3538b
rouche-q <quentinrouchet.dev@gmail.com>
2020-12-15 18:02:51 +0100

    [+] WIP Button

---
+++ src/component/Button/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from "prop-types"
+import "./Button.css"
+
+const Button = ({ label, type, onClick}) => {
+    type = type ? type : "primary"
+
+    return (
+        <button className={"uinator-button " + type} onClick={onClick}>{label}</button>
+    )
+}
+
+Button.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+export { Button }
---
+++ src/component/Button/Button.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {Button }from './Button';
+
+const story = {
+  title: 'UInator/Button',
+  component: Button,
+};
+
+export default story
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Button',
+};
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+  label: 'Button',
+  type: "inverted"
+};
+
+export const Colored = Template.bind({});
+Colored.args = {
+  label: 'Button',
+  type: "green"
+};
+
